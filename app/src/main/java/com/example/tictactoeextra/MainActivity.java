286069a282282aa2f5fdaea402df6c5334217e8a
Krystian Gałwa <krystian.galwa1@gmail.com>
2019-03-13 22:25:04 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/tictactoeextra/MainActivity.java
+++ app/src/main/java/com/example/tictactoeextra/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.net.Uri;
 
 public class MainActivity extends AppCompatActivity  {
 
@@ -21,6 +22,16 @@ public class MainActivity extends AppCompatActivity  {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, GameActivity.class);
                 startActivity(intent);
+            }
+        });
+        Button button1 = findViewById(R.id.open_url);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://www.google.com";
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
             }
         });
 
